f75b8e87617b721800504020df7a5f20d3aa321c
Alexander Wolf <alexanderwolf@outlook.at>
2023-03-19 20:47:20 +0100

    website redesign

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+'use strict';
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
